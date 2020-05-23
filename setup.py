b885c80f3d517caacb79f018c4876f605e1232c2
T. Rogers <tr7200@users.noreply.github.com>
2020-05-23 10:27:16 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,7 @@ from setuptools import setup
 
 setup(name="AdamToSGD",
       version="0.1",
+      description='Tensorflow-Keras callback implementing arXiv 1712.07628',
+      url='http://github.com/tr7200/Adam_to_SGD',
+      license='MIT',
       packages=["AdamToSGD"])
